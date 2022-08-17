b0d30f3c800896379e81920d0284699e01747e50
git-illite <71469298+git-illite@users.noreply.github.com>
2022-08-17 16:50:11 -0400

    updated enviroment files to include APIBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   clientID: "69cfd3d26d1b40098d4694fc16a4333a",
-  clientSecret: "9a039f6d0a1c48cfa203cba6f10432d8"
+  clientSecret: "9a039f6d0a1c48cfa203cba6f10432d8",
+  userAPIBase: 'https://still-hollows-26017.herokuapp.com/api/user',
 };
